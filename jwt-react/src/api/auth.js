0204f1f471abdb6effe178cbd580e3a75c07dafb
Daniel Mouris <dgmouris@gmail.com>
2020-09-03 18:26:27 -0600

    Made small changes to api auth for better testing

--- jwt-react/src/api/auth.js
+++ jwt-react/src/api/auth.js
@@ -6,8 +6,6 @@ const BASE_URL = "http://localhost:8000"
 const ACCESS_TOKEN = 'access_token'
 const REFRESH_TOKEN = 'refresh_token'
 
-let tokenStorage = window.localStorage;
-
 let tokenRequest = axios.create({
     baseURL: BASE_URL,
     timeout: 5000,
@@ -22,35 +20,28 @@ const loginUser = (username, password) => {
 
   return tokenRequest.post(`/api/token/both/`, loginBody)
     .then((response)=> {
-      console.log(response)
-      tokenStorage.setItem(ACCESS_TOKEN, response.data.access);
-      tokenStorage.setItem(REFRESH_TOKEN, response.data.refresh);
+      window.localStorage.setItem(ACCESS_TOKEN, response.data.access);
+      window.localStorage.setItem(REFRESH_TOKEN, response.data.refresh);
       return Promise.resolve(response.data);
     }).catch((error)=>{
       return Promise.reject(error);
-    })
+    });
 }
 
 const refreshToken = () => {
-  const refreshBody = {"refresh": tokenStorage.getItem(REFRESH_TOKEN)}
+  const refreshBody = {"refresh": window.localStorage.getItem(REFRESH_TOKEN)}
   return tokenRequest.post(`/api/token/access/`, refreshBody)
     .then((response)=> {
-      tokenStorage.setItem(ACCESS_TOKEN, response.data.access);
-      return Promise.resolve(response);
+      window.localStorage.setItem(ACCESS_TOKEN, response.data.access);
+      return Promise.resolve(response.data);
     }).catch((error)=>{
       return Promise.reject(error);
-    })
-}
-
-const logout = () => {
-  tokenStorage.removeItem(ACCESS_TOKEN)
-  tokenStorage.removeItem(REFRESH_TOKEN)
+    });
 }
 
 const isCorrectRefreshError = (status) => {
   return status === 401;
 }
-
 
 /*
  * authRequest
@@ -58,7 +49,7 @@ const isCorrectRefreshError = (status) => {
  * This refreshes the request and retries the token if it is invalid.
  * This is what you use to create any requests that need the Tokens.
  * Reference: https://hackernoon.com/110percent-complete-jwt-authentication-with-django-and-react-2020-iejq34ta
- * 
+ *
  * Example:
  *     authRequest.get('/path/to/endpoint/',extraParameters)
  *        .then(response=>{
@@ -71,34 +62,41 @@ const authRequest = axios.create({
     baseURL: BASE_URL,
     timeout: 5000,
     headers: {
-      'Authorization': `Bearer ${tokenStorage.getItem(ACCESS_TOKEN)}`,
+      'Authorization': `Bearer ${window.localStorage.getItem(ACCESS_TOKEN)}`,
       'Content-Type': 'application/json',
     }
 });
 authRequest.interceptors.response.use(
   (response) => response, // this is for all successful requests.
   (error) => { //handle the request
-    const originalRequest = error.config;
-    const status = error.response.status;
-    if (isCorrectRefreshError(status)) {
-      return refreshToken().then((data)=> {
-        const headerAuthorization = `Bearer ${tokenStorage.getItem(ACCESS_TOKEN)}`;
-        authRequest.defaults.headers['Authorization'] = headerAuthorization;
-        originalRequest.headers['Authorization'] = headerAuthorization;
-        return authRequest(originalRequest)
-      }).catch((error)=> {
-        console.log(error)
-      })
-    }
-    return Promise.reject(error)
+    return errorInterceptor(error)
   }
 );
 
+const errorInterceptor = (error) => {
+  // console.log(error.config)
+  // console.log(error.response)
+  const originalRequest = error.config;
+  const status = error.response.status;
+  if (isCorrectRefreshError(status)) {
+    return refreshToken().then((data)=> {
+      const headerAuthorization = `Bearer ${window.localStorage.getItem(ACCESS_TOKEN)}`;
+      authRequest.defaults.headers['Authorization'] = headerAuthorization;
+      originalRequest.headers['Authorization'] = headerAuthorization;
+      return authRequest(originalRequest)
+    }).catch((error)=> {
+      // Note If you are using browser router you can redirect here
+    })
+  }
+  // Note If you are using browser router you can redirect here as well.
+  return Promise.reject(error)
+}
 
 const logoutUser = () => {
-  tokenStorage.removeItem(ACCESS_TOKEN);
-  tokenStorage.removeItem(REFRESH_TOKEN);
+  window.localStorage.removeItem(ACCESS_TOKEN);
+  window.localStorage.removeItem(REFRESH_TOKEN);
   authRequest.defaults.headers['Authorization'] = "";
 }
 
-export { loginUser, logoutUser, refreshToken, authRequest }
+export { tokenRequest, loginUser, logoutUser, refreshToken, authRequest,
+         errorInterceptor, BASE_URL, ACCESS_TOKEN, REFRESH_TOKEN }
