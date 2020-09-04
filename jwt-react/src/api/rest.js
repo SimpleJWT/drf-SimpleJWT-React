5b931678475100a599750cbedc75adf09bdd72dc
Daniel Mouris <dgmouris@gmail.com>
2020-09-04 14:38:12 -0600

    Removed unnecessary error thrown

--- jwt-react/src/api/rest.js
+++ jwt-react/src/api/rest.js
@@ -4,9 +4,6 @@ const ping = () => {
   const extraParameters = {params: {"id": "PONG" }}
   return authRequest.get('/api/ping/',extraParameters)
     .then(response=>{
-      if (response === undefined) {
-        throw new Error("User has not logged in yet")
-      }
       return Promise.resolve(response.data)
     }).catch((error)=> {
       return Promise.reject(error)
